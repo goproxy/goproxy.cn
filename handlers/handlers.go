2490a6e2284b6021c0927c065bb0b3299cd33836
Aofei Sheng <aofei@aofeisheng.com>
2019-04-28 23:29:27 +0800

    refactor: redirect index page requests to GitHub temporarily
    The index page is under development... coming soon...

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -21,5 +21,6 @@ func init() {
 
 // indexPageHandler handles requests to get index page.
 func indexPageHandler(req *air.Request, res *air.Response) error {
-	return res.Render(nil, "index.html")
+	// return res.Render(nil, "index.html")
+	return res.Redirect("https://github.com/goproxy/goproxy.cn")
 }
